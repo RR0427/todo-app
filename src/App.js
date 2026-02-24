bc5e449db004c70637cf50d50135bc51520eb3cb
Rajesh <rairajesh2552@gmail.com>
2026-02-24 15:23:07 +0000

    wrap up for Todo frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
+
   const addTodo = (text) => {
     setTodos([...todos, { id: Date.now(), text, completed: false }]);
   };
 
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div>
-      <h1>Todo List App</h1>;
+      <h1>Todo List App</h1>
       <TodoForm onAdd={addTodo} />
+      <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
     </div>
   );
 }
